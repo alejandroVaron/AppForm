e354cd2345c376e6315c963d51070d59ad09f61f
alejandroVaron <alejandro.varon@correounivalle.edu.co>
2021-11-07 15:10:48 -0500

    fix connection with database

---
+++ backend/config/formatJs/config.js
@@ -0,0 +1,19 @@
+const configuration = {
+  development: {
+    username: "postgres",
+    password: "postgres",
+    database: "postgres",
+    host: "localhost",
+    dialect: "postgres",
+    operatorsAliases: 0,
+  },
+  production: {
+    username: "sxbclbngsapmui",
+    password: "11f3aab0549103303eafdb99bb3ec15299a81089c3d5e8b295acc708ebbe672e",
+    database: "d3eprhoae6bq7p",
+    host: "ec2-3-230-61-252.compute-1.amazonaws.com",
+    dialect: "postgres",
+  },
+};
+
+export const config = configuration;
--- backend/models/index.js
+++ backend/models/index.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-import config from "../config/config.json";
+import { config } from "../config/formatJs/config.js";
 var database = 1; // 0 = Localhost database  ||  1 = Heroku database
 let sequelizeOb;
 if (database == 0) {
@@ -17,24 +17,24 @@ if (database == 0) {
     }
   );
 } else {
-  
-    sequelizeOb = new Sequelize(
-      config.production.database,
-      config.production.username,
-      config.production.password,
-      {
-        host: config.production.host,
-        port: 5432,
-        dialect: "postgres",
-        ssl: 'Amazon RDS',
-        pool: { maxConnections: 5, maxIdleTime: 30 },
-        language: "en",
-        define: {
-          freezeTableName: true,
+  sequelizeOb = new Sequelize(
+    config.production.database,
+    config.production.username,
+    config.production.password,
+    {
+      host: config.production.host,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
         },
-      }
-    );
-
+      },
+      define: {
+        freezeTableName: true,
+      },
+    }
+  );
 }
 
 export const sequelize = sequelizeOb;
